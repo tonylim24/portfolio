aaef83f8af52e1cd477f5e1c10989919295891c1
Tony Lim <tonolim24@gmail.com>
2020-12-23 16:21:31 -0800

    Set up Laravel and React Environment. Also added OOP Layouts.

---
+++ resources/views/layouts/top-nav.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="/#">Tony Lim Portfolio</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://gitlab.com/tonylimmvp" target="_blank" rel="noreferrer">Gitlab</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://linkedin.com/in/tonylim39" target="_blank" rel="noreferrer">LinkedIn</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="http://localhost:7888/portfolio/public/rng" target="_blank" rel="noreferrer">RNG</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="/#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Guides
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="/#">Git</a>
+                    <a class="dropdown-item" href="/#">React</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="/#">MAMP</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="body-container">
+        <div id="example">
+            {{-- React DOM Manipulation: js/components/Index.js --}}
+        </div>
+    </div>
+
+    <div id="bottom-navbar">
+        {{-- React DOM Manipulation: js/components/Footer.js --}}
+    </div>
+    
+@endsection

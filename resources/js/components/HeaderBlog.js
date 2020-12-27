aee3af98539b560a58a43c945f5fe2c30126c98b
Tony Lim <tonolim24@gmail.com>
2020-12-27 15:22:52 -0800

    Removed react-router-dom from HeaderBlog.js

--- resources/js/components/HeaderBlog.js
+++ resources/js/components/HeaderBlog.js
@@ -5,12 +5,11 @@ import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 class HeaderBlog extends Component {
     render() {
         return (
-            <Router>
                 <div>
                     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <Link to="/" class="navbar-brand">
+                        <a href="/" class="navbar-brand">
                             <img src="https://i.imgur.com/rPSBLdw.png" width="30" height="30" alt="" />
-                        </Link>
+                        </a>
                         <p class="navbar-brand"> | Blog</p>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -18,19 +17,16 @@ class HeaderBlog extends Component {
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav">
                                 <li class="nav-item active">
-                                    <Link to="/" class="nav-link">Return to Intro </Link>
+                                    <a href="/" class="nav-link">Return to Intro </a>
                                 </li>
 
                                 <li class="nav-item active">
-                                    <Link to="/blog" class="nav-link">Blog Index </Link>
+                                    <a href="/blog" class="nav-link">Blog Index </a>
                                 </li>
                             </ul>
                         </div>
                     </nav>
                 </div>
-                <Route exact path="/" />
-                <Route exact path="/blog" />
-            </Router>
         );
     }
 }

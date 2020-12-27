c25ea5394c2ba390c75517928b49723b2d6d3cd5
Tony Lim <tonolim24@gmail.com>
2020-12-27 15:26:00 -0800

    Removed react-router-dom, using laravel routes instead since we use laravel for view page.

--- resources/js/components/HeaderBlog.js
+++ resources/js/components/HeaderBlog.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Link, Redirect, Route} from 'react-router-dom';
 
 class HeaderBlog extends Component {
     render() {
         return (
-                <div>
-                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <a href="/" class="navbar-brand">
-                            <img src="https://i.imgur.com/rPSBLdw.png" width="30" height="30" alt="" />
-                        </a>
-                        <p class="navbar-brand"> | Blog</p>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item active">
-                                    <a href="/" class="nav-link">Return to Intro </a>
-                                </li>
+            <div>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a href="/" class="navbar-brand">
+                        <img src="https://i.imgur.com/rPSBLdw.png" width="30" height="30" alt="" />
+                    </a>
+                    <p class="navbar-brand"> | Blog</p>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="/" class="nav-link">Return to Intro </a>
+                            </li>
 
-                                <li class="nav-item active">
-                                    <a href="/blog" class="nav-link">Blog Index </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                            <li class="nav-item active">
+                                <a href="/blog" class="nav-link">Blog Index </a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         );
     }
 }
--- resources/js/components/HeaderMain.js
+++ resources/js/components/HeaderMain.js
@@ -1,50 +1,47 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
 
 class HeaderMain extends Component {
     render() {
         return (
             <div>
-                <Router>
-                    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                        <Link to="/">
-                            <img class="navbar-brand" src="https://i.imgur.com/rPSBLdw.png" width="30" height="30" alt="" />
-                        </Link>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav">
-                                <li class="nav-item active">
-                                    <Link to="/" class="nav-link">
-                                        Intro <span class="sr-only">(current)</span>
-                                    </Link>
-                                </li>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <a href="/">
+                        <img class="navbar-brand" src="https://i.imgur.com/rPSBLdw.png" width="30" height="30" alt="" />
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item active">
+                                <a to="/" class="nav-link">
+                                    Intro <span class="sr-only">(current)</span>
+                                </a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#resume">Resume</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#resume">Resume</a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#projects">Projects</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#projects">Projects</a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#blog">Blog</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#blog">Blog</a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="https://gitlab.com/tonylimmvp" target="_blank">GitLab</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://gitlab.com/tonylimmvp" target="_blank">GitLab</a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="https://github.com/tonylim24/" target="_blank">Github</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </Router>
+                            <li class="nav-item">
+                                <a class="nav-link" href="https://github.com/tonylim24/" target="_blank">Github</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         );
     }
